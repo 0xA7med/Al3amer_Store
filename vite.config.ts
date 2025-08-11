e1fea6587b2a5f47054d577a330d053fafeaaabe
Ahmed Mohamed <zeero4123@gmail.com>
2025-08-11 12:30:02 +0300

    v1.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
